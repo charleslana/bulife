f3b76caa8b9d879070290d1d390f7ee67a35ebe5
Charles <charleslanop@gmail.com>
2022-01-23 12:15:24 -0300

    update script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
+import config from './config';
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
-  console.log('Print successfully!');
+  await page.setViewport({ width: 1200, height: 720 });
+  await page.goto('https://www.ulife.com.br', { waitUntil: 'networkidle0' });
+  await page.type('#txtLogin', config.username);
+  await page.type('#txtPassword', config.password);
+  await page.click('#imbLogin');
+  await page.waitForNavigation();
+  console.log('New page URL:', page.url());
+  const [response] = await Promise.all([
+    page.waitForResponse(response =>
+      response.url().includes('/Calendar/GetCalendarByMonth')
+    ),
+  ]);
+  console.log(await response.json());
   await browser.close();
 })();
